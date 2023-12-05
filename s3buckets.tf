44b82ba89d01393bfc8a92c921a1676b03524865
Marcos <mgonzalezgallo@gmail.com>
2023-12-05 20:20:02 -0300

    file updated

--- s3buckets.tf
+++ s3buckets.tf
@@ -3,10 +3,10 @@
 
 resource "aws_s3_bucket" "example" {
   
-  bucket = "${terraform.workspace}-webapp-ort-devops-2023s2g1-1901"
+  bucket = "${terraform.workspace}-webapp-ort-devops-2023s2g1-2019"
 
   tags = {
     Environment = "${terraform.workspace}"
-    bucketname  = "${terraform.workspace}-webapp-ort-devops-2023s2g1-1901"
+    bucketname  = "${terraform.workspace}-webapp-ort-devops-2023s2g1-2019"
   }
 }
