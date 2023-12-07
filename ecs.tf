7c91aa75a1c6517d417bf893eb0f35bb17e113ca
Marcos <mgonzalezgallo@gmail.com>
2023-12-07 15:24:08 -0300

    capacity provider name modified

--- ecs.tf
+++ ecs.tf
@@ -2,8 +2,8 @@ resource "aws_ecs_cluster" "ecs_cluster" {
     name = "ecs-cluster"
 }
 
-resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
-    name = "ecs-capacity-provider"
+resource "aws_ecs_capacity_provider" "capacity_provider" {
+    name = "nombre-capacity-provider"
 
     auto_scaling_group_provider {
     auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
@@ -20,12 +20,12 @@ resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
 resource "aws_ecs_cluster_capacity_providers" "example" {
     cluster_name = aws_ecs_cluster.ecs_cluster.name
 
-    capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+    capacity_providers = [aws_ecs_capacity_provider.capacity_provider.name]
 
     default_capacity_provider_strategy {
         base              = 1
         weight            = 100
-        capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+        capacity_provider = aws_ecs_capacity_provider.capacity_provider.name
     }
 }
 
