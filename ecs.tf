46130d1cee330948e020c7581807039a8b9f9aaf
Marcos <mgonzalezgallo@gmail.com>
2023-12-07 15:42:04 -0300

    files updated

--- ecs.tf
+++ ecs.tf
@@ -83,7 +83,7 @@ resource "aws_ecs_service" "ecs_service" {
 
     load_balancer {
         target_group_arn = aws_lb_target_group.ecs_tg.arn
-        container_name   = "ecs-alb"
+        container_name   = "orders"
         container_port   = 80
     }
 
