21cae1595dece791efee0599550151cdf50593aa
Gonzalo <gonza_ferpac@hotmail.com>
2023-12-08 13:11:07 -0300

    cambio en rol y en cpu

--- ecs_tasks_definitions.tf
+++ ecs_tasks_definitions.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_task_definition" "orders_task_definition" {
     family             = "ecs-task"
     network_mode       = "awsvpc"
     execution_role_arn = var.ecs_task_execution_role_arn
-    cpu                = 2
+    cpu                = 256
     runtime_platform {
         operating_system_family = "LINUX"
         cpu_architecture        = "X86_64"
@@ -11,7 +11,7 @@ resource "aws_ecs_task_definition" "orders_task_definition" {
     {
         name      = "orders"
         image     = "ghcr.io/ort-devops-2023s2g1/orders-service-example:7090894486"
-        cpu       = 2
+        cpu       = 256
         memory    = 256
         essential = true
         portMappings = [
@@ -34,7 +34,7 @@ resource "aws_ecs_task_definition" "payments_task_definition" {
     family             = "ecs-task"
     network_mode       = "awsvpc"
     execution_role_arn = var.ecs_task_execution_role_arn
-    cpu                = 2
+    cpu                = 256
     runtime_platform {
         operating_system_family = "LINUX"
         cpu_architecture        = "X86_64"
@@ -43,7 +43,7 @@ resource "aws_ecs_task_definition" "payments_task_definition" {
     {
         name      = "orders"
         image     = "ghcr.io/ort-devops-2023s2g1/payments-service-example:7107677406"
-        cpu       = 2
+        cpu       = 256
         memory    = 256
         essential = true
         portMappings = [
@@ -66,7 +66,7 @@ resource "aws_ecs_task_definition" "products_task_definition" {
     family             = "ecs-task"
     network_mode       = "awsvpc"
     execution_role_arn = var.ecs_task_execution_role_arn
-    cpu                = 2
+    cpu                = 256
     runtime_platform {
         operating_system_family = "LINUX"
         cpu_architecture        = "X86_64"
@@ -75,7 +75,7 @@ resource "aws_ecs_task_definition" "products_task_definition" {
     {
         name      = "orders"
         image     = "ghcr.io/ort-devops-2023s2g1/products-service-example:7080022031"
-        cpu       = 2
+        cpu       = 256
         memory    = 256
         essential = true
         portMappings = [
@@ -98,7 +98,7 @@ resource "aws_ecs_task_definition" "shipments_task_definition" {
     family             = "ecs-task"
     network_mode       = "awsvpc"
     execution_role_arn = var.ecs_task_execution_role_arn
-    cpu                = 2
+    cpu                = 256
     runtime_platform {
         operating_system_family = "LINUX"
         cpu_architecture        = "X86_64"
@@ -107,7 +107,7 @@ resource "aws_ecs_task_definition" "shipments_task_definition" {
     {
         name      = "orders"
         image     = "ghcr.io/ort-devops-2023s2g1/shipping-service-example:7080018234"
-        cpu       = 2
+        cpu       = 256
         memory    = 256
         essential = true
         portMappings = [
