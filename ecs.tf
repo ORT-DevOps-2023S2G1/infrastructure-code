24d45ca86c9573ba4bb9047af6cd61e82ae95a1a
Marcos <mgonzalezgallo@gmail.com>
2023-12-08 15:49:02 -0300

    files updated

--- ecs.tf
+++ ecs.tf
@@ -7,32 +7,32 @@ resource "aws_ecs_cluster" "ecs_cluster" {
     }
 }
 
-resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
-    name = "${local.infra_env}-ecs-capacity-provider"
+# resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+#     name = "${local.infra_env}-ecs-capacity-provider"
 
-    auto_scaling_group_provider {
-    auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+#     auto_scaling_group_provider {
+#     auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
 
-    managed_scaling {
-        maximum_scaling_step_size = 1000
-        minimum_scaling_step_size = 1
-        status                    = "ENABLED"
-        target_capacity           = 3
-    }
-    }
-}
+#     managed_scaling {
+#         maximum_scaling_step_size = 1000
+#         minimum_scaling_step_size = 1
+#         status                    = "ENABLED"
+#         target_capacity           = 3
+#     }
+#     }
+# }
 
-resource "aws_ecs_cluster_capacity_providers" "cluster_capacity_provider" {
-    cluster_name = aws_ecs_cluster.ecs_cluster.name
+# resource "aws_ecs_cluster_capacity_providers" "cluster_capacity_provider" {
+#     cluster_name = aws_ecs_cluster.ecs_cluster.name
 
-    capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+#     capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
 
-    default_capacity_provider_strategy {
-        base              = 1
-        weight            = 100
-        capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
-    }
-}
+#     default_capacity_provider_strategy {
+#         base              = 1
+#         weight            = 100
+#         capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+#     }
+# }
 
 resource "aws_ecs_task_definition" "products_task_definition" {
     family             = "ecs-task"
@@ -43,7 +43,7 @@ resource "aws_ecs_task_definition" "products_task_definition" {
         operating_system_family = "LINUX"
         cpu_architecture        = "X86_64"
     }
-    
+
     container_definitions = jsonencode([
     {
         name      = "products"
@@ -73,19 +73,19 @@ resource "aws_ecs_service" "ecs_service" {
         security_groups = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
     }
 
-    force_new_deployment = true
-        placement_constraints {
-            type = "distinctInstance"
-    }
+    # force_new_deployment = true
+    #     placement_constraints {
+    #         type = "distinctInstance"
+    # }
 
-    triggers = {
-        redeployment = timestamp()
-    }
+    # triggers = {
+    #     redeployment = timestamp()
+    # }
 
-    capacity_provider_strategy {
-        capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
-        weight            = 100
-    }
+    # capacity_provider_strategy {
+    #     capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+    #     weight            = 100
+    # }
 
     load_balancer {
         target_group_arn = aws_lb_target_group.ecs_tg.arn
@@ -93,5 +93,5 @@ resource "aws_ecs_service" "ecs_service" {
         container_port   = 80
     }
 
-    depends_on = [aws_autoscaling_group.ecs_asg]
+    # depends_on = [aws_autoscaling_group.ecs_asg]
 }
